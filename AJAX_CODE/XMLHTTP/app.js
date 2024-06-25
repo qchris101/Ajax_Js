c538cbdb056336e76767d1cba5e13a69c3063356
qchris101 <qchris101@yahoo.com>
2024-06-25 14:33:32 -0400

    Working on fetch API lesson.

--- AJAX_CODE/XMLHTTP/app.js
+++ AJAX_CODE/XMLHTTP/app.js
@@ -1,17 +1,15 @@
 // NOT AT ALL IMPORTANT TO REMEMBER ANY OF THIS CODE!
 
-const req = new XMLHttpRequest();
-
-req.onload = function () {
-  console.log("IT LOADED!!");
-  const data = JSON.parse(this.responseText);
-  console.log(data.name, data.height);
-};
-
+const req = new XMLHttpRequest()
+req.onload = function(){
+  console.log("It Loaded!")
+ const data = JSON.parse(this.responseText)
+ console.log(data.name, data)
+}
 req.onerror = function () {
-  console.log("ERROR!!!!");
-  console.log(this);
-};
+  console.log("Error!!")
+  console.log(this)
+}
 
-req.open("GET", "https://swapi.dev/api/people/1/");
-req.send();
+req.open("GET", "https://swapi.dev/api/people/1/")
+req.send()
