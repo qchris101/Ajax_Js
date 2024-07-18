866f52272934699e2a120deca7702e273972395a
qchris101 <qchris101@yahoo.com>
2024-07-18 15:13:37 -0400

    Working on get calls practice with axios.

--- AJAX_CODE/Axios/app.js
+++ AJAX_CODE/Axios/app.js
@@ -1,20 +1,26 @@
-// axios
-//   .get("https://swapi.dev/api/people/1/")
-//   .then((res) => {
-//     console.log("RESPONSE: ", res);
-//   })
-//   .catch((e) => {
-//     console.log("ERROR! ", e);
-//   });
 
+// simple axios DOC call to practice 
+axios.get("https://swapi.dev/api/people/1/").then ((res) => {
+  console.log("Not part of the async function ", res)
+})
+.catch((e) => {
+  console.log("Error: ", e)
+})
+
+// making a axios fetch request with async
 const getStarWarsPerson = async (id) => {
-  try {
-    const res = await axios.get(`https://swapi.dev/api/people/${id}/`);
-    console.log(res.data);
-  } catch (e) {
-    console.log("ERROR", e);
-  }
-};
-
-getStarWarsPerson(5);
-getStarWarsPerson(10);
+  try { // In the try block we have axios with a get request being held by the variable response 
+    const response = await axios.get(`https://swapi.dev/api/people/${id}/`)
+    // This console.log is returning the our response variable with our data 
+  console.log(response.data)
+  // Catch block to return any errors that may ensue.
+ } catch(e){
+   console.log(e)
+ }
+}
+// since we added an id we can search any number from the database
+getStarWarsPerson(25)
+// Created a simple for loop to play around with getting multiple get calls at once.
+for( let i = 1; i<10; i++ ) {
+  getStarWarsPerson(i)
+}
