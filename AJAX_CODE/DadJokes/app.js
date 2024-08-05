a980db02b091a25f55af8737dbc5308786330ff2
qchris101 <qchris101@yahoo.com>
2024-08-05 15:20:39 -0400

    Worked on Dad joke api using axios and async functions.

--- AJAX_CODE/DadJokes/app.js
+++ AJAX_CODE/DadJokes/app.js
@@ -1,4 +1,30 @@
-const getDadJoke = async () => {
-  const config = {headers: {}}
-  const res = await axios.get('https://icanhazdadjoke.com/')
+
+// Selecting the element we want to manipulate
+const jokes = document.querySelector("#jokes")
+const button = document.querySelector('#jokeButton')
+
+const addNewJoke = async () => {
+  const jokeText = await getDadJoke()
+  const newLI = document.createElement('li')
+  // We now need to append(add) our response data (the joke) to our new li that we previously created
+  newLI.append(jokeText)
+  // // now we append to our selected element tagged with the jokes id in our html
+  jokes.append(newLI)
 }
+
+const getDadJoke = async () => {
+  try {
+
+    // Setting Headers following what the documentation is asking for
+    const config = {headers: {Accept: 'application/json',}}
+    // Axios call with url that is needed to call also passing config which is our headers
+    const res = await axios.get('https://icanhazdadjoke.com/', config)
+    // console.log(res.data.joke)
+    // This will return our joke data
+    return res.data.joke
+  } catch (e){
+    return ("No jokes are available!")
+  }
+  }
+
+button.addEventListener('click',addNewJoke)
